8e854289ac9d1e771bdbc567822c348827c40c92
Brinkmann <brinkmann@chm-lin-004753.mpib-berlin.mpg.de>
2020-08-14 13:02:06 +0200

    Cleanup network neighbors.

--- aci/envs/adversial_graph_coloring.py
+++ aci/envs/adversial_graph_coloring.py
@@ -67,33 +67,49 @@ class AdGraphColoring():
         self.reset(init=True)
 
     def to_dict(self):
+        # agent_pos: [c, a, b], agent c is positioned on node 0
         return {
             'graph': list(nx.to_edgelist(self.graph)),
-            'agent_pos': self.agent_pos.tolist()
+            'node_of_agent': self.agent_pos.tolist(),
+            'agent_at_node': self.node_agent.tolist()
         }
+
+
+    def calc_neighbors(self, random_pos=False):
+        if random_pos:
+            self.agent_pos = th.randperm(self.all_agents)
+        else:
+            self.agent_pos = th.arange(self.all_agents)
+        
+        self.node_agent = th.argsort(self.agent_pos)
+
+        node_neighbors = [
+            list(self.graph[i].keys())
+            for i in range(len(self.graph))
+        ]
+        neighbors = [
+            self.node_agent[node_neighbors[ap]][th.randperm(len(node_neighbors[ap]))]
+            for ap in self.agent_pos
+        ]
+        self.neighbors = th.stack(neighbors).to(self.device) # n_agents, n_neighbors
+
 
     def new_graph(self):
         self.graph, n_actions, self.n_neighbors = create_graph(
             n_nodes=self.all_agents, **self.graph_args)
         self.graph_pos = nx.spring_layout(self.graph)
-        self.adjacency_matrix = nx.to_numpy_matrix(self.graph)
-
-        self.neighbors = th.tensor([
-            random.sample(self.graph[n].keys(), len(self.graph[n])) 
-            for n in range(len(self.graph))
-        ], dtype=th.int64, device=self.device) # n_agents, n_neighbors
 
         ci_obs_shape = (self.n_neighbors + 1,)
         ci_agents = self.all_agents - self.fixed_agents
         if self.ai_obs_mode == 'neighbors':
             ai_obs_shape = ci_obs_shape
             ai_agents = ci_agents
-        elif self.ai_obs_mode == 'agents_only':
-            ai_obs_shape = (self.all_agents,)
-            ai_agents = 1
-        elif self.ai_obs_mode == 'agents_matrix':
-            ai_obs_shape = (self.all_agents,(self.all_agents, self.all_agents))
-            ai_agents = None
+        # elif self.ai_obs_mode == 'agents_only':
+        #     ai_obs_shape = (self.all_agents,)
+        #     ai_agents = 1
+        # elif self.ai_obs_mode == 'agents_matrix':
+        #     ai_obs_shape = (self.all_agents,(self.all_agents, self.all_agents))
+        #     ai_agents = None
         else:
             raise NotImplementedError(f'AI observation mode is not implemented yet.')
 
@@ -119,7 +135,8 @@ class AdGraphColoring():
         elif self.ai_obs_mode == 'agents_only':
             ai_obs = self.state['ci'].unsqueeze(0)
         elif self.ai_obs_mode == 'agents_matrix':
-            ai_obs = [(self.state['ci'], self.adjacency_matrix)]
+            raise NotImplementedError()
+            # ai_obs = [(self.state['ci'], self.adjacency_matrix)]
         else:
             raise NotImplementedError(f'AI observation mode is not implemented yet.')
 
@@ -132,8 +149,7 @@ class AdGraphColoring():
         conflicts = (observations['ci'][:,[0]] == observations['ci'][:,1:])
 
         ind_coordination = 1 - conflicts.any(dim=1).type(th.float)
-        ind_catch = (
-            self.state['ai'][self.agent_pos] == self.state['ci'][self.agent_pos]).type(th.float)
+        ind_catch = (self.state['ai'] == self.state['ci']).type(th.float)
 
         metrics = {
             'ind_coordination': ind_coordination,
@@ -142,16 +158,11 @@ class AdGraphColoring():
             'avg_catch': ind_catch.mean(0, keepdim=True).expand(self.n_agents['ci']),
         }
 
-        # cat = [metrics[k] * v for k,v in self.rewards_args['ci'].items()]
-        # cat = th.cat(cat)
         ci_reward = th.stack([metrics[k] * v for k,v in self.rewards_args['ci'].items()]).sum(0)
-
         ai_reward = th.stack([metrics[k] * v for k,v in self.rewards_args['ai'].items()]).sum(0)
         
-        if self.n_agents['ai'] == 1:
+        if self.ai_obs_mode == 'agents_matrix':
             ai_reward = ai_reward.sum(0, keepdim=True)            
-
-        # assert ai_reward.shape[0] == 1
 
         reward = {
             'ai': ai_reward,
@@ -162,10 +173,7 @@ class AdGraphColoring():
 
 
     def step(self, actions):
-        if 'ai' in actions:
-            self.state['ai'] = actions['ai']
-        if 'ci' in actions:
-            self.state['ci'][self.agent_pos] = actions['ci']
+        self.state = actions
 
         observations = self.observations()
         rewards, info = self.get_rewards(observations)
@@ -176,65 +184,33 @@ class AdGraphColoring():
         else:
             done = False
         self.steps += 1
-
-        # if writer:
-        #     self._log(actions, observations, info, done, writer) 
-
         return observations, rewards, done, info
 
     def reset(self, init=False):
         if not self.fixed_network or init:
             self.new_graph()
+        if not self.fixed_pos or init:
+            self.calc_neighbors(random_pos=not self.fixed_pos)
+
         self.steps = 0
 
-        self.agg_metrics = {}
+        if self.fixed_agents:
+            # TODO: current fixed agents are not supported
+            raise NotImplementedError()
+            # get a solution
+            # _state = get_graph_colors(self.graph)
+        else:
+            # random starting state
+            ci_start_state = th.tensor(
+                np.random.randint(0, self.n_actions['ci'], self.n_agents['ci']), dtype=th.int64)
+            ai_start_state = th.tensor(
+                np.random.randint(0, self.n_actions['ai'], self.n_agents['ai']), dtype=th.int64)
 
-        # TODO: fix agent_pos
-        # if not self.fixed_pos or init:
-        #     self.agent_pos = np.random.choice(
-        #         self.all_agents, self.n_agents['ci'], replace=False)
-        self.agent_pos = np.arange(self.all_agents)
-
-        _state = get_graph_colors(self.graph)
         self.state = {
-            'ci': _state,
-            'ai': _state
+            'ci': ci_start_state,
+            'ai': ai_start_state
         }
-        self.state['ci'][self.agent_pos] = th.tensor(
-            np.random.randint(0, self.n_actions['ci'], self.n_agents['ci']), dtype=th.int64)
-        self.state['ai'][self.agent_pos] = th.tensor(
-            np.random.randint(0, self.n_actions['ai'], self.n_agents['ci']), dtype=th.int64)
         return self.observations()
-
-    def _render(self, state, metrics):
-        fig = plt.figure()
-
-        labels = {i: f"{int_to_alphabete(i)}" for i in range(len(self.graph))}
-        labels2 = {
-            i: f"{int_to_alphabete(i)}:{r:.2f}" for i,r in zip(self.agent_pos, metrics['ci']) }
-        labels = {**labels, **labels2}
-        
-        node_color_ci = [state['ci'][i].item() for i in self.graph.nodes()]        
-        node_color_ai = [state['ai'][i].item() for i in self.graph.nodes()]
-        nx.draw(self.graph, self.graph_pos, node_color=node_color_ai, node_size=500)
-        nx.draw(
-            self.graph, self.graph_pos, node_color=node_color_ci, 
-            labels=labels, node_size=200, edgelist=[], font_size=20, font_color='magenta')
-
-        plt.text(
-            0, 0, f"#{self.steps} Corr {metrics['avg_coordination']} Catch {metrics['avg_catch']}", 
-            fontsize=30, color='magenta'
-        )
-        # plt.text(-0.8, -0.5, f"s: {[i.item() for i in state['ci']]}", fontsize=15)
-        # plt.text(-0.8, -0.7, f"nc ci: {node_color_ci}", fontsize=15)
-        # plt.text(-0.8, -0.9, f"nc ai: {node_color_ai}", fontsize=15)
-        fig.canvas.draw()
-        # plt.savefig('temp.png')
-        data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-        data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-        data = np.moveaxis(data, 2, 0)
-        plt.close()
-        return th.tensor(data[np.newaxis, np.newaxis])
 
     def close(self):
         pass
