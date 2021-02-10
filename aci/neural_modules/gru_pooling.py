c43234149b8acb226de5bbd329758b423aaabd2e
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2021-02-10 20:40:02 +0100

    Refactor gru pooling.

--- aci/neural_modules/gru_pooling.py
+++ aci/neural_modules/gru_pooling.py
@@ -4,8 +4,30 @@ import torch as th
 
 
 class PoolingGRUAgent(nn.Module):
+    """
+    A agent based on two sandwich of linear and recurrent layer.
+
+    Args:
+        n_neighbors: Number of (potential) neighbors. This includes the agent itself.
+        view_size: Size of the view encoding.
+        control_size: Size of the control encoding.
+        hidden_size: Size of the hidden encoding.
+        n_actions: Number of actions.
+        merge_pos: Position in the layer stack at which to merge the control encoding.
+        merge_type: Strategy used to merge the control encoding.
+
+    Indices:
+        a: actions [0..n_actions]
+        s: episode step [0..episode_steps (+ 1)]
+        b: batch idx [0..batch_size]
+        n: neighbors [0..max_neighbors (+ 1)]
+        i: view encoding [0..n_view]
+        c: control encoding [0..n_control]
+        h: hidden encoding
+    """
+
     def __init__(
-            self, n_inputs, input_size, control_size, n_actions, hidden_size, pooling_types, 
+            self, n_neighbors, view_size, control_size, n_actions, hidden_size, pooling_types,
             linear1, rnn1, linear2, rnn2, device, merge_pos=None, merge_type=None):
         super(PoolingGRUAgent, self).__init__()
         self.pooling_types = pooling_types
@@ -13,88 +35,125 @@ class PoolingGRUAgent(nn.Module):
         self.merge_pos = merge_pos
         self.merge_type = merge_type
 
-        next_in = input_size
+        n_hidden = view_size
         if linear1:
-            self.linear1 = nn.Linear(in_features=next_in, out_features=hidden_size)
-            next_in = hidden_size
+            self.linear1 = nn.Linear(in_features=n_hidden, out_features=hidden_size)
+            n_hidden = hidden_size
         else:
             self.linear1 = None
 
         if rnn1:
-            self.rnn1 = nn.GRU(input_size=next_in, hidden_size=hidden_size, batch_first=True)
-            next_in = hidden_size
+            self.rnn1 = nn.GRU(input_size=n_hidden, hidden_size=hidden_size, batch_first=True)
+            n_hidden = hidden_size
         else:
             self.rnn1 = None
-        
+
         # pooling
-        next_in = next_in*(len(pooling_types) + 1)
-        
+        n_hidden = n_hidden*(len(pooling_types) + 1)
+
         if merge_pos == 'prelin2':
-            next_in = merged_size(next_in, control_size, merge_type)
+            n_hidden = merged_size(n_hidden, control_size, merge_type)
         if linear2:
-            self.linear2 = nn.Linear(in_features=next_in, out_features=hidden_size)
-            next_in = hidden_size
+            self.linear2 = nn.Linear(in_features=n_hidden, out_features=hidden_size)
+            n_hidden = hidden_size
         else:
             self.linear2 = None
         if merge_pos == 'prernn2':
-            next_in = merged_size(next_in, control_size, merge_type)
+            n_hidden = merged_size(n_hidden, control_size, merge_type)
         if rnn2:
             self.rnn2 = nn.GRU(
-                input_size=next_in, batch_first=True, 
+                input_size=n_hidden, batch_first=True,
                 hidden_size=hidden_size)
-            next_in = hidden_size
+            n_hidden = hidden_size
         else:
             self.rnn2 = None
         if merge_pos == 'prelin3':
-            next_in = merged_size(next_in, control_size, merge_type)
-        self.linear3 = nn.Linear(in_features=next_in, out_features=n_actions)
+            n_hidden = merged_size(n_hidden, control_size, merge_type)
+        self.linear3 = nn.Linear(in_features=n_hidden, out_features=n_actions)
 
     def reset(self):
         self.hidden1 = None
         self.hidden2 = None
 
-    def forward(self, x, mask, control=None):
-        batch_size, seq_length, n_inputs, input_size = x.shape # n_inputs: neighbors + self      
-        x = x.permute(0,2,1,3) # batch, n_inputs, seq_length, input_size
-        mask = ~mask.permute(0,2,1).unsqueeze(-1) # batch, n_inputs, seq_length, 1
+    def forward(self, view, mask, control=None):
+        """
+        Args:
+            view: The view of the agent on itself and other agents. A 4-D `Tensor` of type
+            `th.float32` and shape `[b,s,n,i]`.
+            mask: A mask indicating valid values in the input. A 3-D `Tensor` of type
+            `th.bool` and shape `[b,s,n]`.
+            control: Additional input to control the agent. A 3-3 `Tensor` of type
+            `th.float32` and shape `[b,s,c]`
+        Indices:
+            a: actions [0..n_actions]
+            s: episode step [0..episode_steps (+ 1)]
+            b: batch idx [0..batch_size]
+            n: neighbors [0..max_neighbors (+ 1)]
+            i: view encoding [0..n_view]
+            c: control encoding [0..n_control]
+            h: hidden encoding
+        """
+        b, s, n, _ = view.shape
+        b2, s2, n2 = mask.shape
+
+        assert b == b2
+        assert s == s2
+        assert n == n2
+
+        view = view.permute(0, 2, 1, 3)  # b, n+, s, i
+        mask = ~mask.permute(0, 2, 1).unsqueeze(-1)  # b, n+, s, *
 
         if self.linear1:
-            x = F.relu(self.linear1(x))
+            view = F.relu(self.linear1(view))  # b*n+, s, h
 
         if self.rnn1:
-            x = x.reshape(batch_size*n_inputs,seq_length,-1) # batch*n_inputs, seq_length, input_size
-            x, self.hidden1 = self.rnn1(x, self.hidden1) # batch*n_inputs, seq_length, hidden_size
-            x = x.reshape(batch_size,n_inputs,seq_length,-1) # batch, n_inputs, seq_length, hidden_size
+            view = view.reshape(b*n, s, -1)  # b*n+, s, h
+            view, self.hidden1 = self.rnn1(view, self.hidden1)  # b*n+, s, h
+            view = view.reshape(b, n, s, -1)  # b, n+, s, h
 
-        x_self = x[:, 0]
-        x_others = x[:, 1:] * mask[:, 1:]
-
-        pooled = [x_self]
-
-        if 'avg' in self.pooling_types:
-            pooled.append(x_others.sum(dim=1) / mask.sum(dim=1)) # batch, seq_length, hidden_size
-        if 'max' in self.pooling_types:
-            pooled.append(x_others.max(dim=1)[0]) # batch, seq_length, hidden_size
-        if 'sum' in self.pooling_types:
-            pooled.append(x_others.mean(dim=1)) # batch, seq_length, hidden_size
-        
-        x = th.cat(pooled, axis=-1)
+        view = pool(view, mask, self.pooling_types)  # b, s, h
 
         if self.merge_pos == 'prelin2':
-            x = merge_control(x, control, self.merge_type)
+            view = merge_control(view, control, self.merge_type)  # b, s, h
         if self.linear2:
-            x = F.relu(self.linear2(x))
+            view = F.relu(self.linear2(view))
         if self.merge_pos == 'prernn2':
-            x = merge_control(x, control, self.merge_type)
+            view = merge_control(view, control, self.merge_type)  # b, s, h
         if self.rnn2:
-            x, self.hidden2 = self.rnn2(x, self.hidden2)
+            view, self.hidden2 = self.rnn2(view, self.hidden2)  # b, s, h
         if self.merge_pos == 'prelin3':
-            x = merge_control(x, control, self.merge_type)
-        q = self.linear3(x)
-        return q
+            view = merge_control(view, control, self.merge_type)  # b, s, h
+        q = self.linear3(view)  # b, s, a
+        return q  # b, s, a
+
+
+def pool(view, mask, pooling_types):
+    """
+    Args:
+        view: `[b, n+, s, i]`
+        mask: `[b, n+, s]`
+    """
+    # the first element in the neighbors dimension is representing the agent itself
+    view_self = view[:, 0]  # b, s, h
+    # the other elements are representing neighbors
+    view_others = view[:, 1:] * mask[:, 1:]  # b, n, s, h
+
+    pooled = [view_self]
+
+    if 'avg' in pooling_types:
+        pooled.append(view_others.sum(dim=1) / mask.sum(dim=1))  # b, s, h
+    if 'max' in pooling_types:
+        pooled.append(view_others.max(dim=1)[0])  # b, s, h
+    if 'sum' in pooling_types:
+        pooled.append(view_others.mean(dim=1))  # b, s, h
+
+    return th.cat(pooled, axis=-1)  # b, s, i
 
 
 def merged_size(hidden_size, control_size, merge_type):
+    """
+    Calculates the hidden size after the merge operation.
+    """
     if merge_type == 'cat':
         return hidden_size + control_size
     elif merge_type == 'outer':
@@ -105,14 +164,20 @@ def merged_size(hidden_size, control_size, merge_type):
         raise NotImplementedError(f'Unkown merge type: {merge_type}')
 
 
-def merge_control(x, control, merge_type):
+def merge_control(view, control, merge_type):
+    """
+    Args
+        view: `[b,s,h]`
+        control: `[b,s,c]`
+    """
     if merge_type == 'cat':
-        return th.cat([x, control], axis=-1)
+        return th.cat([view, control], axis=-1)  # b,s,h+c
     elif merge_type == 'outer':
-        return th.einsum('ijk,ijl->ijkl', x, control).reshape(x.shape[0],x.shape[1],-1)
+        return th.einsum('ijk,ijl->ijkl', view, control) \
+            .reshape(view.shape[0], view.shape[1], -1)  # b,s,h*c
     elif merge_type == 'outer_normalized':
-        control_normed = control / control.sum(dim=-1, keepdim=True)
-        x = th.einsum('ijk,ijl->ijkl', x, control_normed).reshape(x.shape[0],x.shape[1],-1)
-        return x
+        control_normed = control / control.sum(dim=-1, keepdim=True)  # b,s,c
+        return th.einsum('ijk,ijl->ijkl', view,
+                         control_normed).reshape(view.shape[0], view.shape[1], -1)  # b,s,h*c
     else:
         raise NotImplementedError(f'Unkown merge type: {merge_type}')
