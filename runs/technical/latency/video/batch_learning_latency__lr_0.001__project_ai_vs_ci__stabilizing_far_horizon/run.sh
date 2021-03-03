488d155b3e4f792cf4bcc9904cc9201326273a59
levin.brinkmann01 <brinkmann@mpib-berlin.mpg.de>
2021-03-03 10:17:28 +0100

    Add runs.

---
+++ runs/technical/latency/video/batch_learning_latency__lr_0.001__project_ai_vs_ci__stabilizing_far_horizon/run.sh
@@ -0,0 +1,16 @@
+#PBS -l nodes=1:ppn=6
+#PBS -N batch_learning_latency__lr_0.001__project_ai_vs_ci__stabilizing_far_horizon
+#PBS -l walltime=2:0:0
+#PBS -l mem=32gb
+#PBS -j oe
+#PBS -o runs/technical/latency/video/batch_learning_latency__lr_0.001__project_ai_vs_ci__stabilizing_far_horizon/log.log
+#PBS -m n
+#PBS -d .
+
+module load python/3.7
+
+source .venv/bin/activate
+
+echo "Entered environment"
+
+video runs/technical/latency/video/batch_learning_latency__lr_0.001__project_ai_vs_ci__stabilizing_far_horizon runs/technical/latency/train/project_ai_vs_ci__batch_learning_latency__stabilizing_far_horizon__lr_0.001
